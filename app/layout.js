ce25e4cddf86b8001a7ccbbe140033a566209c5f
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-12-17 18:54:21 +0530

    [ADD]: Supabase and Inngest added!

--- app/layout.js
+++ app/layout.js
@@ -2,18 +2,23 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/mycomponents/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Welth",
   description: "One Stop Finance Platform",
+  icon: "/favicon.ico",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        {/* <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head> */}
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
