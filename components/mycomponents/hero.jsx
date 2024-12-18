54f4766e63eacd84a1738cc4027ab2e8c51a4da4
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-12-18 21:54:24 +0530

    [ADD]: Prisma tables added

--- components/mycomponents/hero.jsx
+++ components/mycomponents/hero.jsx
@@ -54,7 +54,7 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        <div className="hero-image-wrapper mt-5 md:">
+        <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
               src="/banner.jpeg"
